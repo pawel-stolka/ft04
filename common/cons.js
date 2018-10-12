c1fd34f39fc9272759ee1ac0e03775ff15ed0ce4
Pablo <pawel.stolka@gmail.com>
2018-10-12 06:11:44 +0200

    locals

--- common/cons.js
+++ common/cons.js
@@ -1,10 +1,12 @@
 const consts = {
-    mongoString: process.env.DB_CONN_STR_QA2,
-    secretString: process.env.SECRET_KEY,
+    mongoString: 'mongodb://pablo:pablo2q@ds245022.mlab.com:45022/auth4',
+    secretString: 'secret blah blah',
 
-    apiUrl: 'https://auth04.herokuapp.com/',
+    apiUrl: 'http://localhost:9999',//'http://localhost:9011',
+    // apiUrl: 'https://authtemplate.herokuapp.com/',
 
-    frontUrl: 'http://pablodev.pl',
+    frontUrl: 'http://localhost:4200',
+    // frontUrl: 'http://pablodev.pl',
 
     port: 9999,
     wsPort: 5555
