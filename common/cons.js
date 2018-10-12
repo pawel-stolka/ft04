e8bb96f4d8d2eebe1e03f5bfcfef3a6a4c06ad95
Pablo <pawel.stolka@gmail.com>
2018-10-12 06:07:36 +0200

    local

--- common/cons.js
+++ common/cons.js
@@ -1,15 +1,15 @@
 const consts = {
-    mongoString: process.env.DB_CONN_STR_QA2,
-    secretString: process.env.SECRET_KEY,
+    mongoString: 'mongodb://pablo:pablo2q@ds245022.mlab.com:45022/auth4',
+    secretString: 'secret blah blah',
 
     apiUrl: 'http://localhost:9999',//'http://localhost:9011',
     // apiUrl: 'https://authtemplate.herokuapp.com/',
 
     frontUrl: 'http://localhost:4200',
     // frontUrl: 'http://pablodev.pl',
+
     port: 9999,
     wsPort: 5555
 }
 
 exports.consts = consts
-// to make a difference
